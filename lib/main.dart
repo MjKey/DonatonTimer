6f07ccce9c0301d4701133f0664fba4b1714b5dd
MjKey <mj.dot.key@gmail.com>
2024-09-10 23:21:47 +0300

    FIx Skip Donate

--- lib/main.dart
+++ lib/main.dart
@@ -654,8 +654,8 @@ class _MainScreenState extends State<MainScreen> {
     var bsys = donationData['billing_system'] ?? 'NOT DATA';
     var bsyt = donationData['billing_system_type'] ?? 'NOT DATA';
     if (donationData['currency'] == 'RUB' ||
-        donationData['is_shown'] == 0 ||
-        donationData['billing_system'] != 'TWITCH' ||
+        donationData['is_shown'] == 0 &&
+        donationData['billing_system'] != 'TWITCH' &&
         donationData['billing_system_type'] != 'REWARDS') {
       double amountMain = donationData['amount_main'].toDouble();
       String username = donationData['username'] ??
